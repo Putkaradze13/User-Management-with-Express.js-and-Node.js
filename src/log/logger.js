064347a5566a1d547858602e63e992d7d59eb240
“Tengiz <t.putkaradze@artjoker.team>
2022-05-03 17:16:58 +0400

    modified logger

--- src/log/logger.js
+++ src/log/logger.js
@@ -1,13 +1,22 @@
-import { pino } from 'pino';
-
-export const logger = pino({
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      levelFirst: true,
-      colorize: true,
-      translateTime: 'SYS:dd-mm-yyyy HH-MM-ss',
-      ignore: 'pid,hostname'
-    }
+import logger from 'pino';
+export class Logger {
+  constructor(filename) {
+    this.log = logger({
+      prettyPrint: {
+        colorize: true,
+        levelFirst: true,
+        translateTime: 'SYS:dd-mm-yyyy HH-MM-ss',
+        ignore: 'pid,hostname'
+      }
+    });
+    this.filename = filename;
   }
-});
+
+  info(msg) {
+    this.log.info(this.filename + ': ' + msg);
+  }
+
+  error(msg) {
+    this.log.error(this.filename + ' ' + msg);
+  }
+}
